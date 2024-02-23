cc56fa0cffe9d721362596e285eed6ce6907e40e
Voupi <voupi.sfh@gmail.com>
2024-02-22 21:43:42 -0600

    struct

--- Models/Mrb.go
+++ Models/Mrb.go
@@ -4,5 +4,17 @@ type MBR struct {
 	Tamano        int32
 	FechaCreacion int64
 	DiskSignature int32
+	Fit           [1]byte
+	Partitions    [4]Partition
 	// EspacioReservado [fileSize - 16]byte // Espacio restante en la estructura
+}
+type Partition struct {
+	Status      [1]byte
+	Type        [1]byte
+	Fit         [1]byte
+	Start       int32
+	Size        int32
+	Name        [16]byte
+	Correlative int32
+	Id          [4]byte
 }
